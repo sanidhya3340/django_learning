2d628df3c258922ea942df91dbb8502127c8fe9d
sanidhya3340 <sanidhyabhardwaj3340@gmail.com>
2022-06-16 14:18:45 +0530

    creating a basic dynamic page

--- calc/views.py
+++ calc/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{'name':'Sanidhya'})
+    return render(request, 'home.html',{'name':'Sanu'})
